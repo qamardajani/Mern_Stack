6a84ec5ed599609e66663c087f87ff12a8da331c
qamardajani <qamar.dajani@gmail.com>
2021-07-13 12:54:48 +0300

    first jsx assingment

---
+++ Mern-React/React_intro/first-jsx/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+                <h1>Hello Dojo!</h1>
+                <h3>Things i want to do:</h3>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+                    
+      </>
+    );
+  }
+}
+export default App;
