efec1ddb71482ca37484bd787c396fb608729919
qamardajani <qamar.dajani@gmail.com>
2021-07-14 14:59:15 +0300

    prop it up and put together

--- Mern-React/React_intro/propit-up/src/App.js
+++ Mern-React/React_intro/propit-up/src/App.js
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
         <PersonCard firstName ="Jane" lastName ="Doe" age = {45} hairColor = "black"/>
         <PersonCard firstName = {"John"} lastName = {"Smith"} age = {88} hairColor = {"Brown"} />
-
         <PersonCard firstName = {"Millard"} lastName = {"Fillmore"} age = {50} hairColor = {"Brown"} />
-
         <PersonCard firstName = {"Maria"} lastName = {"Smith"} age = {62} hairColor = {"Brown"} />
 
     </div>
--- Mern-React/React_intro/propit-up/src/components/PersonCard.js
+++ Mern-React/React_intro/propit-up/src/components/PersonCard.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
     
     class PersonCard extends Component {
-        render() {
+        constructor(props) {
+            super(props);
+            this.state = {
+            age: this.props.age
+            };
+        }
+            ageCounter = () => {this.setState({age:this.state.age+1})}
+        render(){ 
             const {firstName,lastName,age,hairColor}= this.props;
             return(
                 <>
-                <h1> {lastName},{firstName}</h1>
-                <p> Age : {age}</p>
-                <p> Hair color : {hairColor}</p>
+                <h1> {this.props.lastName},{this.props.firstName}</h1>
+                <p> Age : {this.state.age}</p>
+                <p> Hair color : {this.props.hairColor}</p>
+                <button onClick={ this.ageCounter} >Birthdaaay</button>
+        
                 </>
-            );
-
+            )
+            
             
         }
     }
+    
     export default PersonCard;
