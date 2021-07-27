f141f768bcd0389cf6e3180e003325e613ee5838
qamardajani <qamar.dajani@gmail.com>
2021-07-27 14:54:25 +0300

    add faker assignment

---
+++ Express/faker/server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const app = express();
+const faker = require("faker");
+
+
+class User {
+  constructor(){
+    this._id = faker.datatype.uuid();
+    this.firstName = faker.name.firstName();
+    this.lastName = faker.name.lastName();
+    this.phoneNumber = faker.phone.phoneNumber();
+    this.email = faker.internet.email();
+    this.password = faker.internet.password();
+
+  }
+}
+console.log(new User());
+
+class Company {
+  constructor(){
+    this._id = faker.datatype.uuid();
+    this.name = faker.company.companyName();
+    this.street = faker.address.streetName();
+    this.city = faker.address.city();
+    this.state = faker.address.state();
+    this.zipCode = faker.address.zipCode();
+    this.country = faker.address.country();
+  }
+}
+console.log(new Company());
+  
+app.get('/api/users/new', (req,res) =>{
+  var user = new User();
+  res.json({
+    results: user
+  });
+  
+})
+
+app.get('/api/companies/new', (req,res) =>{
+  var company = new Company();
+  res.json({
+    results: company
+  });
+  
+})
+
+app.get('/api/user/company', (req, res) =>{
+  var user = new User();
+  var company = new Company();
+  res.json({
+    results: {
+      user: user,
+      company: company,
+    }
+  })
+})
+
+
+const server = app.listen(8000, () =>
+  console.log(`Server is locked and loaded on port ${server.address().port}!`)
+);
